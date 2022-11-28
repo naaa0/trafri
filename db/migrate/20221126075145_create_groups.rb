7a79e3721c162a55046c434bc4e20ec01e2e5e52
naaa0 <lil.nuts70@gmail.com>
2022-11-28 14:47:27 +0900

    groupsテーブル作成

---
+++ db/migrate/20221126075145_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[6.0]
+  def change
+    create_table :groups do |t|
+      t.string  :name,           null: false
+      t.integer :prefecture_id,  null: false
+      t.string  :city,           null: false
+      t.text    :introduction,   null: false
+      t.timestamps
+    end
+  end
+end
