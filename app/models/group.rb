b1e0b803aac352497095a94ae042e5b6da72b45a
naaa0 <lil.nuts70@gmail.com>
2022-11-28 19:12:35 +0900

    グループ新規作成機能の実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
-  has_many :user_groups
-  has_many :users, through: :user_groups
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
