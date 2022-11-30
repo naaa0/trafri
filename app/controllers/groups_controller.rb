97e8af9b5ddd12685772207db33bbe5fdc1ee453
naaa0 <lil.nuts70@gmail.com>
2022-11-30 15:55:22 +0900

    グループ編集と削除機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :find_group, only: [:show, :edit, :update, :destroy]
+  before_action :correct_group, only: [:edit, :destroy]
 
   def index
     @group = Group.all
@@ -19,8 +21,24 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_path(@group.id)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to root_path
+  end
+
 
   private
 
@@ -28,4 +46,11 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:image, :name, :introduction, :prefecture_id, :city).merge(user_id: current_user.id)
   end
 
+  def find_group
+    @group = Group.find(params[:id])
+  end
+
+  def correct_group
+    redirect_to root_path unless @group.user.id == current_user.id
+  end
 end
