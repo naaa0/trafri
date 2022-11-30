72e56d8e3bfd78a9c38d1411eef6ba9b2a263290
naaa0 <lil.nuts70@gmail.com>
2022-11-30 17:11:56 +0900

    groupテーブルのバリデーション追記

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,14 @@
 class Group < ApplicationRecord
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :city
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
 end
