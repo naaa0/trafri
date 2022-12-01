c5b84c4aa2ed8834df9c4c12d8c37f34d7ded9cb
naaa0 <lil.nuts70@gmail.com>
2022-12-01 16:50:26 +0900

    チャット機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
   end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group.id)
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  has_one_attached :image
 end
